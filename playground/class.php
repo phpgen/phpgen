8d11255bf8a7c1e8925eac9ae9126e6a9d60d191
localusercamp <user062199@gmail.com>
2023-11-20 00:13:25 +0500

    chore: Add some class examples

--- playground/class.php
+++ playground/class.php
@@ -3,7 +3,7 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use PHPGen\Builders\ClassBuilder;
-
+use PHPGen\Builders\MethodBuilder;
 
 echo ClassBuilder::fromAnonymous('MyClass', new class {
     public function hello() 
@@ -16,3 +16,16 @@ echo ClassBuilder::fromAnonymous('MyClass', new class {
         return $s + $isBool;
     }
 });
+
+echo "\n\n";
+
+echo ClassBuilder::make('Cat')
+    ->final()
+    ->methods([
+        MethodBuilder::fromClosure('myau', function (): void {
+            echo 'myau';
+        }),
+        MethodBuilder::fromClosure('eatFood', function (float $amount): void {
+            // eat some food
+        }),
+    ]);
